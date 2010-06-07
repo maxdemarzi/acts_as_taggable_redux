c8c73d947afa6bfbeee8bcac19afad8062f95a49
maxdemarzi <maxdemarzi@hotmail.com>
2010-06-07 13:05:52 -0700

    require helper

--- lib/acts_as_taggable_redux.rb
+++ lib/acts_as_taggable_redux.rb
@@ -1,9 +1,24 @@
-require 'acts_as_taggable'
-require 'acts_as_tagger'
+require "active_record"
+require "action_view"
 
-ActiveRecord::Base.send(:include, ActiveRecord::Acts::Taggable)
-ActiveRecord::Base.send(:include, ActiveRecord::Acts::Tagger)
-ActionView::Base.send(:include, ActsAsTaggableHelper)
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'tagging'
-require 'tag'
+require "acts_as_taggable"
+require "acts_as_tagger"
+require "acts_as_taggable_helper"
+
+require "tagging"
+require "tag"
+
+$LOAD_PATH.shift
+
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.send :include, ActiveRecord::Acts::Taggable
+  ActiveRecord::Base.send :include, ActiveRecord::Acts::Tagger
+end
+
+if defined?(ActionView::Base)
+  ActionView::Base.send :include, ActsAsTaggableHelper
+end
+
+
